0aac9ee50fd7d208199a4869f31b0abf713c6766
Bumbatumbarumba <kosakowb@mcmaster.ca>
2018-04-09 15:46:08 -0400

    created Screens.java to create the different windows needed

---
+++ src/code/Screens.java
@@ -0,0 +1,14 @@
+package code;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Screens extends JPanel{
+	
+}//end of Screens
+
+
+@SuppressWarnings("serial")
+class PlayArea extends JPanel{
+	
+}
