2b1ba95d458970c334f82f9f7e97c7b504ac5386
Bumbatumbarumba <kosakowb@mcmaster.ca>
2018-04-10 00:05:50 -0400

    testing stuff out with adding a character (the player) to the screen and drawing them

--- src/code/Main.java
+++ src/code/Main.java
@@ -4,9 +4,28 @@
  */
 package code;
 
-public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.IOException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		JFrame test = new JFrame("test");
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.setVisible(true);
+		test.setSize(500, 500);
+		test.setLocationRelativeTo(null);
+		test.setResizable(false);
+		
+		Player play = new Player(200, 200, 0, 0, "src/images/player.png");
+		JLabel lbl = new JLabel(new ImageIcon("../src/images/player.png"));
+		lbl.setVisible(true);
+		lbl.setLocation(50, 50);
+		
+		test.add(play);
+		test.add(lbl);
 	}//end of main
 }//end of Main
