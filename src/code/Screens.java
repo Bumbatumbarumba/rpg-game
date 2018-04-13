4e0836667300dc2ec4e350ba7e3e7c93bc9fd00a
Bumbatumbarumba <kosakowb@mcmaster.ca>
2018-04-12 21:22:59 -0400

    added keylistener and actionlistener to allow for player movement

--- src/code/Screens.java
+++ src/code/Screens.java
@@ -2,71 +2,152 @@ package code;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class Screens extends JPanel{
-	public static void initPlayAreas(){
-		PlayArea area1 = new PlayArea(200,200, 1000, 1000);
-		System.out.println("area generated");
-		try {
-			int counter = 5;
-			System.out.println("waiting 5 second");
-			while (counter > 0){
-				System.out.println(counter);
-				--counter;
-				Thread.sleep(1000);
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("making area visible");
+	private static PlayArea area1;
+	private static PlayArea area2;
+	public static InventoryScreen playerInv = new InventoryScreen();
+	
+	public static void runScreens(){
+		initPlayAreas();
 		area1.changeVis(true);
 	}
+	
+	public static void initPlayAreas(){
+		area1 = new PlayArea(200,200, 500, 500);
+		area2 = new PlayArea(200,200, 500, 1000);
+	}//end of initPlayAreas
+	
+	
 }//end of Screens
 
 
 @SuppressWarnings("serial")
-class PlayArea extends JPanel implements ActionListener{
-	private JFrame test = new JFrame("test");
+class PlayArea extends JPanel implements ActionListener, KeyListener{
+	private JFrame playArea = new JFrame("test");
 	private Timer t = new Timer(10, this);
+	private Player player = new Player(100, 100, 0, 0);
 	
 	//the x and y are the starting location of the player so that
 	//when we create new screens we can easily position the player
 	public PlayArea(int x, int y, int w, int h){
 		
+		System.out.println("TEST1");
+		
+		//let's us do stuff with keyboard input or something lol
+		this.playArea.addKeyListener(this);
+		this.playArea.setFocusable(true);
+		
+		//start timer to respond to keyboard input
+		t.start();
+		
 		//creates the play area
-		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		test.setSize(w, h);
-		test.setLocationRelativeTo(null);
-		test.setResizable(false);
+		this.playArea.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.playArea.setSize(w, h);
+		this.playArea.setLocationRelativeTo(null);
+		this.playArea.setResizable(false);
 		
 		//creates an instance of player
-		Player player;
-		try {
-			player = new Player(x, x, 0, 0, "src/images/player.png");
-			test.add(player);
+		this.playArea.add(this.player);
+		
+		/*try {
+			this.player = new Player(x, x, 1, 1);
+			
+			System.out.println("INSIDE CONSTRUCTOR");
+			
+			this.playArea.add(this.player);
 		} catch (IOException e) {
 			//only thrown if the game can't find the player image for whatever reason
 			System.out.println("couldn't find no gosh darn image file");
-		}
+		}*/
 		
 		//NOTE TO SELF: ALWAYS MAKE THE JFRAME VISIBLE LAST!!!!
 		//default will be false
-		test.setVisible(false);
+		this.playArea.setVisible(false);
 	}//end of constructor
 	
 	//change the visibility of the play area as needed
 	public void changeVis(boolean val){
-		this.test.setVisible(val);
+		this.playArea.setVisible(val);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		this.player.playerTick();
+		this.playArea.repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent k) {
+		switch (k.getKeyCode()){
+		case KeyEvent.VK_W:
+			this.player.setDy(-2);
+			break;
+		case KeyEvent.VK_S:
+			this.player.setDy(2);
+			break;
+		case KeyEvent.VK_A:
+			this.player.setDx(-2);
+			break;
+		case KeyEvent.VK_D:
+			this.player.setDx(2);
+			break;	
+		case KeyEvent.VK_J:
+			System.out.println("test");
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent k) {
+		switch (k.getKeyCode()){
+		case KeyEvent.VK_W:
+			this.player.setDy(0);
+			break;
+		case KeyEvent.VK_S:
+			this.player.setDy(0);
+			break;
+		case KeyEvent.VK_A:
+			this.player.setDx(0);
+			break;
+		case KeyEvent.VK_D:
+			this.player.setDx(0);
+			break;		
+		}	
+	}
+
+	@Override
+	public void keyTyped(KeyEvent k) {
+		//prolly won't need this
 	}
 }//end of PlayArea class
+
+//window that will come up and display the user's inventory 
+@SuppressWarnings("serial")
+class InventoryScreen extends JPanel implements KeyListener{
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}//end of InventoryScreen class
