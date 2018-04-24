bbd6fc5e3344feead639edd89e733df8f8407146
Bumbatumbarumba <kosakowb@mcmaster.ca>
2018-04-24 17:48:10 -0400

    created class for the starting menu of the game

---
+++ src/code/MainMenu.java
@@ -0,0 +1,13 @@
+/* Created by Bartosz Kosakowski
+ * April 20th, 2018
+ * Stores stuff related to creating the main menu and its functionality
+ */
+package code;
+
+import javax.swing.JPanel;
+
+//CHANGE THE TEXTURES OF ALL OF THE BUTTONS, BACKGROUND, TITLE, ETC
+@SuppressWarnings("serial")
+public class MainMenu extends JPanel{
+
+}//end of main menu
